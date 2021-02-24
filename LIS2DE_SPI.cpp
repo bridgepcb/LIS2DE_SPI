5dcb376a1f5b8444088a0ed72118e1f8a3a29844
Leonardo Márquez <leonardoemarq@gmail.com>
2021-02-24 11:54:50 -0300

    Update LIS2DE_SPI.cpp

--- LIS2DE_SPI.cpp
+++ LIS2DE_SPI.cpp
@@ -1,5 +1,5 @@
 /*
-  LIS2DE_SPI.c - Arduino library for the LIS2DE accelerometer / digital motion sensor from STMicroelectronics.
+  LIS2DE_SPI.cpp - Arduino library for the LIS2DE accelerometer / digital motion sensor from STMicroelectronics.
   Created by Leonardo Marquez, September 28, 2020.
   Bridge PCB Design: http://www.bridgepcbdesign.com
 */
